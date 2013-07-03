60b2293da1cd19aa6748718f2c16e8d57722c824
oFF_rus <oFF_rus@98a6af76-d760-e011-b5d3-001517e85bc2>
2013-07-03 21:45:56 +0000

    + Experimental code for animation decoding + export files without palletes to RAW binaries + import raw binaries for second file format
    git-svn-id: svn://svn.old-games.ru/translate@187 98a6af76-d760-e011-b5d3-001517e85bc2

--- projects/KingKong10/src/ru/old_games/kingkong10/ImageCodec.java
+++ projects/KingKong10/src/ru/old_games/kingkong10/ImageCodec.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import net.sf.image4j.codec.bmp.BMPDecoder;
 import net.sf.image4j.codec.bmp.BMPEncoder;
 
+import org.apache.commons.io.input.CountingInputStream;
 import org.apache.commons.io.input.SwappedDataInputStream;
 
 public class ImageCodec {
@@ -50,7 +51,12 @@ public class ImageCodec {
 				System.out.println("decoding rle image: "+args[1]);
 				decode5(args[1]);				
 			}
-			
+			if (args[0].equals("/6" )) {
+				//    /6 TWP00.PZM
+				System.out.println("decoding rle animation: "+args[1]);
+				decode6(args[1]);				
+			}
+
 			if (args[0].equals("/e1" )) {
 				//    /e1 MAINSCR.000.bmp MAINSCR.000 
 				//    /e1 OPTION.bmp OPTION
@@ -61,7 +67,13 @@ public class ImageCodec {
 			if (args[0].equals("/e2" )) {
 				//    /e2 MAINSCR.001.bmp 
 				System.out.println("encoding rle image: "+args[1]);
-				encode2(args[1]);				
+				encode2(args[1], false);				
+			}
+
+			if (args[0].equals("/er2" )) {
+				//    /e2 MAINSCR.001.bmp 
+				System.out.println("encoding rle image: "+args[1]);
+				encode2(args[1], true);				
 			}
 
 			if (args[0].equals("/e3" )) {
@@ -137,6 +149,10 @@ public class ImageCodec {
 		}
 		
 		byte[] decodedImage = rleDecode(data, rleMarker);
+		
+		FileOutputStream fos = new FileOutputStream(new File(rleFile.getAbsolutePath()+".raw"));
+		fos.write(decodedImage);
+		fos.close();
 
 		BufferedImage image = new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_RGB);
 
@@ -174,6 +190,11 @@ public class ImageCodec {
 		//System.out.println(result.size());
 		for (int i=0; i<result.size(); i++) {
 			OneFrame frame = result.get(i);
+			
+			FileOutputStream fos = new FileOutputStream(new File(rleFile.getAbsolutePath()+"."+i+".raw"));
+			fos.write(frame.getImage());
+			fos.close();
+
 			
 			BufferedImage image = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_RGB);
 			sdis = new SwappedDataInputStream(new ByteArrayInputStream(frame.getImage()));
@@ -220,7 +241,9 @@ public class ImageCodec {
 			}
 		}
 		
-		BMPEncoder.write(image, new File(rleFile.getAbsolutePath()+".bmp"));		
+		BMPEncoder.write(image, new File(rleFile.getAbsolutePath()+".bmp"));
+		
+		sdis.close();
 	}
 
 	private static void decode5(String srcFileName) throws IOException {
@@ -271,6 +294,174 @@ public class ImageCodec {
 		
 		BMPEncoder.write(image, new File(srcFile.getAbsolutePath()+".1"+".bmp"));		
 		
+	}
+
+	private static void decode6(String fileName) throws EOFException, IOException {
+		File srcFile = new File(fileName);
+
+		CountingInputStream counter = new CountingInputStream(new FileInputStream(srcFile));
+		SwappedDataInputStream sdis = new SwappedDataInputStream(counter);
+
+		int frameCount = sdis.readInt();
+		System.out.println("frames[4b]: "+frameCount);
+		
+		int width = sdis.readUnsignedShort();
+		int height = sdis.readUnsignedShort();
+		System.out.println("frame size[2b+2b]: "+width+"x"+height);
+		
+		int fileSize = sdis.readInt();
+		System.out.println("internal file size[4b]: "+fileSize+", real file size:"+srcFile.length());
+		
+		int unknown1 = sdis.readInt();
+		System.out.println("unknown1[4b] = "+unknown1);
+		
+		int unknown2 = sdis.readInt();
+		System.out.println("unknown2[4b] = "+unknown2);
+		
+		int unknown3 = sdis.readUnsignedByte();
+		System.out.println("unknown3[1b] = "+unknown3);
+
+		int unknown4 = sdis.readInt();
+		System.out.println("unknown4[4b] = "+unknown4);
+		
+		int unknown5 = sdis.readInt();
+		System.out.println("unknown5[4b] = "+unknown5);
+		
+		byte[] pal = new byte[256*3];
+		sdis.readFully(pal);
+		int[] colors = pal2color(pal);
+		System.out.println("pal read, size: 256*3");
+		
+		// first frame data
+		int rleMarker = sdis.readUnsignedByte();
+		System.out.println("rle marker[1b] = "+rleMarker);
+		
+		int fullFrameSize = sdis.readInt();
+		System.out.println("fullFrameSize[4b] = "+fullFrameSize);
+		
+		int frameWidth = sdis.readUnsignedShort();
+		int frameHeight = sdis.readUnsignedShort();		
+		System.out.println("frame size[2b+2b]: "+frameWidth+"x"+frameHeight);
+		
+		byte[] data = new byte[fullFrameSize - 1 - 4 - 2 - 2];
+		sdis.readFully(data);
+
+		System.out.println("Position in file: 0x"+Integer.toHexString(counter.getCount()).toUpperCase());
+		
+		byte[] decodedImage = rleDecode(data, rleMarker);
+		
+		System.out.println("frame size should be: "+(frameWidth*frameHeight)+" but calculated: "+decodedImage.length);
+
+		BufferedImage image = new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB);
+
+		SwappedDataInputStream sdis2 = new SwappedDataInputStream(new ByteArrayInputStream(decodedImage));
+		for (int currentY = 0; currentY < frameHeight; currentY++) {
+			for (int currentX = 0; currentX < frameWidth; currentX++) {
+				image.setRGB(currentX, currentY, colors[sdis2.readUnsignedByte()]);
+			}			
+		}
+		
+		BMPEncoder.write(image, new File(srcFile.getAbsolutePath()+".0.bmp"));
+		System.out.println("file "+srcFile.getAbsolutePath()+".0.bmp saved.");
+		
+		// second frame
+		System.out.println("======= second frame");
+		
+		int unknown = sdis.readUnsignedByte();
+		System.out.println("unknown1[1b]: "+unknown);
+
+		unknown = sdis.readInt();
+		System.out.println("unknown2[4b]: "+unknown);
+		
+		unknown = sdis.readInt();
+		System.out.println("unknown3[4b]: "+unknown);
+
+		unknown = sdis.readUnsignedByte();
+		System.out.println("unknown4[1b]: "+unknown);
+		
+		unknown = sdis.readInt();
+		System.out.println("unknown5[4b]: "+unknown);
+		
+		unknown = sdis.readInt();
+		System.out.println("unknown6[4b]: "+unknown);
+		
+		unknown = sdis.readUnsignedByte();
+		System.out.println("unknown7[1b]: "+unknown);
+		
+		int offsetX = sdis.readUnsignedByte();
+		System.out.println("offset x[1b]: "+offsetX);
+		
+		int offsetY = sdis.readUnsignedByte();
+		System.out.println("offset y[1b]: "+offsetY);
+		
+		unknown = sdis.readUnsignedShort();
+		System.out.println("unknown8[2b]: "+unknown);
+		
+		unknown = sdis.readInt();
+		System.out.println("unknown9[4b]: "+unknown);
+		
+		rleMarker = sdis.readUnsignedByte();
+		System.out.println("rle marker[1b] = "+rleMarker);
+		
+		fullFrameSize = sdis.readInt();
+		System.out.println("fullFrameSize[4b] = "+fullFrameSize);
+		
+		frameWidth = sdis.readUnsignedShort();
+		frameHeight = sdis.readUnsignedByte();		
+		System.out.println("frame size[2b+1b]: "+frameWidth+"x"+frameHeight);
+		
+		data = new byte[fullFrameSize - 1 - 4 - 2 - 1];
+		sdis.readFully(data);
+		
+		System.out.println("Position in file: 0x"+Integer.toHexString(counter.getCount()).toUpperCase());
+		
+		decodedImage = rleDecode(data, rleMarker);
+		
+		System.out.println("frame size should be: "+(frameWidth*frameHeight)+" but calculated: "+decodedImage.length);
+
+		image = new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_RGB);
+
+		sdis2 = new SwappedDataInputStream(new ByteArrayInputStream(decodedImage));
+		for (int currentY = 0; currentY < frameHeight; currentY++) {
+			for (int currentX = 0; currentX < frameWidth; currentX++) {
+				image.setRGB(currentX, currentY, colors[sdis2.readUnsignedByte()]);
+			}			
+		}
+		
+		BMPEncoder.write(image, new File(srcFile.getAbsolutePath()+".1.bmp"));
+		System.out.println("file "+srcFile.getAbsolutePath()+".1.bmp saved.");
+
+		
+		sdis.close();
+		
+		
+/*
+		int rleMarker = sdis.readUnsignedByte();		
+		
+		int currentPosition = 1+4;
+		byte[] data = new byte[imgEndPosition - currentPosition];
+		sdis.readFully(data);
+		
+		byte[] pal = new byte[3*256];
+		sdis.readFully(pal);
+	
+		sdis.close();
+		
+		int[] colors = pal2color(pal);
+		
+		byte[] decodedImage = rleDecode(data, rleMarker);
+
+		BufferedImage image = new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_RGB);
+
+		sdis = new SwappedDataInputStream(new ByteArrayInputStream(decodedImage));
+		for (int currentY = 0; currentY < maxHeight; currentY++) {
+			for (int currentX = 0; currentX < maxWidth; currentX++) {
+				image.setRGB(currentX, currentY, colors[sdis.readUnsignedByte()]);
+			}			
+		}
+		
+		BMPEncoder.write(image, new File(srcFile.getAbsolutePath()+".bmp"));
+	*/		
 	}
 
 	private static void encode1(String bmpFileName, String origFileName) throws IOException {
@@ -334,39 +525,54 @@ public class ImageCodec {
 		fos.close();	
 	}
 
-	private static void encode2(String bmpFileName) throws IOException {
-		int[] colors = new int[256];
-		for (int i=0; i<colors.length; i++) {
-			colors[i] = 0xFF000000 | (i * 0x10000 + i * 0x100 + i);			
-		}
+	private static void encode2(String bmpFileName, boolean isRaw) throws IOException {
 
-		BufferedImage image = BMPDecoder.read(new File(bmpFileName));
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();		
-
-		for (int y=0; y<maxHeight; y++) {
-			for (int x=0; x<maxWidth; x++) {
-				int realColor = -1;
-				int currentColor = image.getRGB(x, y);
-				for (int j=0; j<256; j++) {
-					if(currentColor == colors[j] ) {
-						realColor = j;
-					}
-				}
-				if (realColor == -1) {
-					throw new IOException("Unknown color 0x"+Integer.toHexString(currentColor)+" in BMP at pos. (x,y): ("+x+","+y+")");
-				}
-				baos.write(realColor);				
-			}			
-		}
-
-		byte[] imageData = baos.toByteArray();
-		
 		int rleMarker = 1;
+		byte[] imageData = null;
+		
+		if (!isRaw) {
+
+			int[] colors = new int[256];
+			for (int i=0; i<colors.length; i++) {
+				colors[i] = 0xFF000000 | (i * 0x10000 + i * 0x100 + i);			
+			}
+
+			BufferedImage image = BMPDecoder.read(new File(bmpFileName));
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();		
+
+			for (int y=0; y<maxHeight; y++) {
+				for (int x=0; x<maxWidth; x++) {
+					int realColor = -1;
+					int currentColor = image.getRGB(x, y);
+					for (int j=0; j<256; j++) {
+						if(currentColor == colors[j] ) {
+							realColor = j;
+						}
+					}
+					if (realColor == -1) {
+						throw new IOException("Unknown color 0x"+Integer.toHexString(currentColor)+" in BMP at pos. (x,y): ("+x+","+y+")");
+					}
+					baos.write(realColor);				
+				}			
+			}
+			
+			imageData = baos.toByteArray();
+		} else {
+			File rawFile = new File(bmpFileName);
+			FileInputStream fis = new FileInputStream(rawFile);
+			imageData = new byte[(int) rawFile.length()];
+			fis.read(imageData);
+			fis.close();
+			rleMarker = 0;
+		}
+
+		
+		
 		
 		imageData = rleEncode(imageData, rleMarker);
 		
-		baos = new ByteArrayOutputStream();		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();		
 		baos.write(rleMarker);
 
 		int filesize = imageData.length+1+4;
@@ -639,6 +845,7 @@ public class ImageCodec {
 						bytesProcessed+=2;
 					} catch (EOFException e) {
 						e.printStackTrace();
+						sdis.close();
 						throw new IOException(e);
 					}
 				} else {
@@ -662,7 +869,7 @@ public class ImageCodec {
 				rleFinished = true;
 			}
 		};		
-		
+		sdis.close();
 		//System.out.println("resultLength="+Integer.toHexString(resultLength));
 		return bytesProcessed;		
 	}
@@ -690,6 +897,7 @@ public class ImageCodec {
 						rleColor = sdis.readUnsignedByte();
 					} catch (EOFException e) {
 						e.printStackTrace();
+						sdis.close();
 						throw new IOException(e);
 					}
 				} else {
@@ -704,7 +912,7 @@ public class ImageCodec {
 				rleFinished = true;
 			}
 		};		
-		
+		sdis.close();
 		return baos.toByteArray();		
 	}
 
@@ -738,6 +946,8 @@ public class ImageCodec {
 		
 			result.add(frame);			
 		} while (sdis.available() > 0);
+		
+		sdis.close();
 
 		return result;
 	}
@@ -748,14 +958,14 @@ public class ImageCodec {
 
 		int rleLength = 0;
 		int rleColor = -1;
-		int readed = 0;
+		//int readed = 0;
 		boolean rleFinished = false;
 
 		while (!rleFinished) {
 			int currentByte = -1;
 			try {
 				currentByte = sdis.readUnsignedByte();
-				readed++;
+				//readed++;
 			} catch (EOFException e) {
 				rleFinished = true;
 			}
