2e8cd45ee30bc252f4f753ecfe3b9977f9185e08
Br. John Fn None <bjohnfn@gmail.com>
2017-08-21 13:14:31 +0300

    hoboken win fix

--- Superhero League of Hoboken/slh_pack.rb
+++ Superhero League of Hoboken/slh_pack.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 begin
-    require 'bundler/setup'
+    #require 'bundler/setup'
     require 'resedit'
     if Resedit::VERSION != '1.4'
         raise LoadError.new("Wrong resedit version")
