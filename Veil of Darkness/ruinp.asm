2c864f842cace67f7c122250dc292d609451b92d
Br. John Fn None <bjohnfn@gmail.com>
2017-10-15 05:11:23 +0300

    vod ru input v1

--- Veil of Darkness/ruinp.asm
+++ Veil of Darkness/ruinp.asm
@@ -1,14 +1,117 @@
+;nasm ruinp.asm -o ruinp.bin
 section .text 
 
-    mov    bx, indexTranslationTable   ; bx = offset of the translation table
-    add    ax, ax                      ; ax *= 2 since each element of the table is 16-bit
-    add    bx, ax 
-    mov    ax, [cs:bx]                 ; new index -> old index
-    neg    ax
-    add    ax, -2
+; func ru_input (al - scancode, bx - eng table)
+ru_input:
+    mov ah, 0
+    cmp byte [cs:keyb_ru], 0
+    je ru_inp_orig
+    cmp al, 0x10
+    jb ru_inp_orig
+    cmp ax, 0x34
+    ja ru_inp_orig
+    sub ax, 0x10
+    mov bx, key_table
+    shl ax, 1
+    add bx, ax
+    mov ax, word [cs:bx]
+    retf
+ru_inp_orig:
+    shl ax, 1
+    add bx, ax
+    mov ax, word [bx]
     retf
 
-indexTranslationTable:
-dw 0
+    nop
+
+; func tolower (al - char)
+ru_lower:
+    mov ah, 0
+    cmp al, 0xF0
+    jne rl_not_yo
+    inc ax
+    retf
+rl_not_yo:
+    add ax, 0x20
+    cmp al, 0xB0
+    jb rl_done
+    add ax, 0x30
+rl_done:
+    retf
+    nop
 
 
+
+keyb_ru db 1
+
+key_table
+dw  0x89 ; 0x10 'Q'
+dw  0x96 ; 0x11 'W'
+dw  0x93 ; 'E'
+dw  0x8A ; 'R'
+dw  0x85 ; 'T'
+dw  0x8D ; 'Y'
+dw  0x83 ; 'U'
+dw  0x98 ; 'I'
+dw  0x99 ; 'O'
+dw  0x87 ; 'P'
+dw  0x95 ; '['
+dw  0x9A ; ']'
+dw  0x000d ; '.'
+dw  0x011d ; '.'
+dw  0x94 ; 'A'
+dw  0x9B ; 'S'
+dw  0x82 ; 0x20 'D'
+dw  0x80 ; 'F'
+dw  0x8F ; 'G'
+dw  0x90 ; 'H'
+dw  0x8E ; 'J'
+dw  0x8B ; 'K'
+dw  0x84 ; 'L'
+dw  0x86 ; ';'
+dw  0x9D ; '''
+dw  0xF0 ; 0x29 '`'
+dw  0x012a ; '*'
+dw  0x005c ; '\'
+dw  0x9F ; 'Z'
+dw  0x97 ; 'X'
+dw  0x91 ; 'C'
+dw  0x8C ; 'V'
+dw  0x88 ; 'B'
+dw  0x92 ; 'N'
+dw  0x9C ; 'M'
+dw  0x81 ; ','
+dw  0x9E ; 0x34 0x2e '.'
+
+;HELPER CHUNK CODE.2
+    nop
+    nop
+    nop
+    nop
+    nop
+    ;122E:113A - readkey
+    mov bx, 0xe17
+    call 0xFFFF:0xFFFF
+    mov si, ax
+    
+    nop
+    ;122E:1162 - peek key
+    mov bx, 0xe17
+    call 0xFFFF:0xFFFF
+
+    nop
+
+min3:
+    int 3    
+    nop
+    ;0050:9AD7 max sym cmd
+    cmp word [bp-0x30], 0x00F1
+    jg min3
+    
+    nop
+    ;0050:A704 max sym name
+    cmp byte [bp-01], 0
+
+    nop
+    ; 19E3:0026  to lower
+    call 0xFFFF:0xFFFF
--- Veil of Darkness/vodka.rb
+++ Veil of Darkness/vodka.rb
@@ -3,85 +3,77 @@
 require 'bundler/setup'
 require 'resedit'
 require 'pathname'
-require 'roo-xls'
 
 
 class PatchCommand < Resedit::AppCommand
     def initialize()
         super(['patch'])
         addParam('file', 'code file to patch (path to code.1 / code.2)')
+        addOption('rebuild', 'r', false, 'rebuild asm')
     end
 
     def swap16(word)
         return ((word & 0xFF) << 8) | ((word>>8) & 0xFF)
     end
 
-    def readXls(fname)
-        xlsname = Pathname.new(fname).parent().join("A.xls").to_s
-        puts "reading table from #{xlsname}"
-        xls = Roo::Spreadsheet.open(xlsname, extension: :xls)
-        sht = xls.sheet(0)
-        rid = 1
-        map = {}
-        while true
-            rw = sht.row(rid)
-            rid+=1
-            break if rw[0]=="===="
-            next if !rw[0] || !rw[1]
-            map[rid-3] = rw[1].downcase()
-        end
-        map = Hash[map.invert().sort()]
-        puts map
-        ret = ""
-        map.each{ |k,v|
-            ret += sprintf("%04X", swap16(v))
-        }
-        #puts "TABLE = "+ret
-        return ret
+    def patchCode2(fname)
+        FileUtils.cp(fname, fname + '.bak') if ! File.exist?(fname + '.bak')
+        patch = Resedit::CodePatch.loadPatch("ruinp.bin")
+        mz = Resedit::MZ.new(fname+".bak")
+        add = mz.replace(patch[0].hexdata())
+        addr = add[1]
+
+        r122e = (0x122e + add[2]).to_s(16)
+        r50 = (0x50 + add[2]).to_s(16)
+        r19e3 = (0x19E3 + add[2]).to_s(16)
+        puts "Patching readkey func -> ru_input"
+        mz.change(r122e+":113A", patch[1].value(0, 10, patch[0].addr(0,addr)))
+        mz.reloc(r122e+":1140")
+
+        puts "Patching peekkey func -> ru_inpu"
+        mz.change(r122e+":1162", patch[1].value(1, 10, patch[0].addr(0,addr)))
+        mz.reloc(r122e+":1168")
+
+        puts "Patch max key compare"
+        mz.change(r50+":9AD7", patch[1].value(3, 9))
+        mz.change(r50+":A704", patch[1].value(4, 4))
+
+        puts "To lower change -> ru_lower"
+        mz.change(r19e3+":0026", patch[1].value(5, 5, patch[0].addr(1,addr)))
+        mz.reloc(r19e3+":0029")
+
+
+
+        #mz.print("changes")
+        mz.print("header")
+        mz.save(fname,"final")
+        mz.close()
     end
 
     def job(params)
+        if params['rebuild']
+            puts "rebuilding asm"
+            raise "Rebuild failed" if !system("nasm ruinp.asm -o ruinp.bin")
+        end
+
         fname = params['file']
         log("Patching #{fname}.")
-        FileUtils.cp(fname, fname + '.bak') if ! File.exist?(fname + '.bak')
+        if fname.downcase.end_with?("code.2")
+            patchCode2(fname)
+        else
+            raise "Don't know how to patch "+fname
+        end
 
-        mz = Resedit::MZ.new(fname)
-        table = readXls(fname)
-        puts "adding translate table code..."
-        code = "bb100001c001c32e8b07f7d805feffcb" + table
-        addr = mz.replace(code)[-1]
-        #mz.dasm("append", "16")
-        puts sprintf("inserting call to %04X:%04X...", addr[1], addr[0])
-        mz.change("0050:AD07", "9A"+sprintf("%04X%04X", swap16(addr[0]), swap16(addr[1])))
-        #mz.dasm("0050:AD04", "14")
-        puts "Adding relocation..."
-        mz.reloc("0050:AD0A")
-
-        #mz.print("header")
-        mz.print("changes")
-        mz.save(fname)
-        mz.close()
 
         log("File %s patched.", fname)
     end
 end
-
-class ExploreCommand < Resedit::AppCommand
-    def initialize()
-        super(['explore'])
-    end
-
-    def job(params)
-    end
-end
-
 
 class App < Resedit::App
     def initialize()
         super('VoD key adjust','0.1',
             [
                 PatchCommand.new(),
-                ExploreCommand.new(),
             ],
             false,
             "by bjfn (c) old-games.ru, 2017")
